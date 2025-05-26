ace0353f306c7d576c1625f8a9e5edf135e96f97
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-05-26 14:10:01 +0600

    add centralized assets file for importing and exporting media resources

---
+++ src/assets/assets.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import dark_logo from "./logo-white.svg";
+import hero_image from "./hero.png";
+import about_image from "./about.png";
+import about_image_2 from "./about-2.png";
+import screen_1 from "./screen-1.png";
+import screen_2 from "./screen-2.png";
+import screen_3 from "./screen-3.png";
+import mobile_frame from "./mobile-frame.png";
+import cta_image from "./cta.png";
+import author_1 from "./author-1.png";
+import author_2 from "./author-2.png";
+import author_3 from "./author-3.png";
+import author_4 from "./author-04.png";
+import blog_image_1 from "./blog-01.jpg";
+import blog_image_2 from "./blog-02.jpg";
+import blog_image_3 from "./blog-03.jpg";
+import client_1 from "./client-01.svg";
+import client_2 from "./client-02.svg";
+import client_3 from "./client-03.svg";
+import client_4 from "./client-04.svg";
+import client_5 from "./client-05.svg";
+import client_6 from "./client-06.svg";
+
+export const assets = {
+  logo,
+  dark_logo,
+  hero_image,
+  about_image,
+  about_image_2,
+  screen_1,
+  screen_2,
+  screen_3,
+  mobile_frame,
+  cta_image,
+  author_1,
+  author_2,
+  author_3,
+  author_4,
+  blog_image_1,
+  blog_image_2,
+  blog_image_3,
+  client_1,
+  client_2,
+  client_3,
+  client_4,
+  client_5,
+  client_6,
+};
