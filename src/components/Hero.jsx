9237ab6ae9af50a2aae5369290746aea56c17fc1
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-05-31 14:34:15 +0600

    feat: add Framer Motion animation to Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { Play } from "lucide-react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { motion } from "framer-motion";
+import { slideRigth } from "../utils/Animation";
 
 const Hero = ({ setShowVideo }) => {
   const { darkMode } = useContext(ThemeContext);
@@ -12,19 +14,39 @@ const Hero = ({ setShowVideo }) => {
         <div className="flex flex-col lg:flex-row items-center gap-12 lg:gap-20">
           {/* Left Content */}
           <div className="w-full lg:w-1/2">
-            <h3 className="text-xl text-gray-700 dark:text-gray-300">
+            <motion.h3
+              variants={slideRigth(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="text-xl text-gray-700 dark:text-gray-300"
+            >
               Ready to Use Tailwind CSS Web Template.
-            </h3>
-            <h1 className="text-4xl sm:text-5xl font-bold mt-4 leading-tight mb-6 text-gray-800 dark:text-white">
+            </motion.h3>
+            <motion.h1
+              variants={slideRigth(0.6)}
+              initial="hidden"
+              animate="visible"
+              className="text-4xl sm:text-5xl font-bold mt-4 leading-tight mb-6 text-gray-800 dark:text-white"
+            >
               Tailwind Template for{" "}
               <span className="text-indigo-400">App & Software</span> Site.
-            </h1>
-            <p className="text-base sm:text-lg text-gray-600 dark:text-gray-400 mb-6">
+            </motion.h1>
+            <motion.p
+              variants={slideRigth(0.7)}
+              initial="hidden"
+              animate="visible"
+              className="text-base sm:text-lg text-gray-600 dark:text-gray-400 mb-6"
+            >
               All-in-one Tailwind CSS site template for App and Software sites.
               Comes with all essential components.
-            </p>
+            </motion.p>
 
-            <div className="flex flex-col sm:flex-row items-start md:items-center gap-5">
+            <motion.div
+              variants={slideRigth(1.0)}
+              initial="hidden"
+              animate="visible"
+              className="flex flex-col sm:flex-row items-start md:items-center gap-5"
+            >
               <button className="bg-[#181c31] hover:opacity-90 dark:bg-white dark:text-black dark:hover:bg-gray-200 text-white px-8 py-4 rounded-md flex items-center gap-3 transition-all font-medium cursor-pointer">
                 <span>Download App</span>
                 <span className="inline-block w-[1px] h-6 bg-gray-400"></span>
@@ -59,11 +81,16 @@ const Hero = ({ setShowVideo }) => {
                   </span>
                 </div>
               </button>
-            </div>
+            </motion.div>
           </div>
 
           {/* Right Image */}
-          <div className="w-full lg:w-1/2 relative flex justify-center mt-10 lg:mt-0">
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", stiffness: 50, delay: 0.2 }}
+            className="w-full lg:w-1/2 relative flex justify-center mt-10 lg:mt-0"
+          >
             <div className="relative z-10">
               <img
                 className="w-full max-w-sm relative"
@@ -82,7 +109,7 @@ const Hero = ({ setShowVideo }) => {
               />
             </div>
             <div className="hidden md:block absolute top-[20px]  w-[500px] h-[500px] z-0 rounded-full bg-gradient-to-tr from-indigo-400 via-purple-300 to-sky-300  opacity-50 pointer-events-none"></div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
